cc22e0dc9a25fb72e0b7f536900d2c6c9b5ad873
Tufancan <68762511+hollmmes@users.noreply.github.com>
2023-04-24 01:57:05 +0300

    Update $-login.py

--- $-login.py
+++ $-login.py
@@ -10,9 +10,45 @@ while a==1:
         if(passw!=sifre):
             print("şifreniz yanlış")
     if(nick==kullanici_adi and passw==sifre):
-        print("Hoş Geldiniz")
+        print("Hoş Geldin Admin")
         a=2
     else:  
         print("Hatalı giriş yaptınız")
+data={}
+while True:
+    id_no= input("Çalışan no: ")
+    if not id_no:
+        break
+    name= input("Çalışan adı:")
+    surname = input("Çalışan soyad: ")
+    income = int(input("Çalışan maaş: "))
+    data.update({ 
+        id_no: {
+            "ad": name,
+            "soyad": surname, 
+            "maaş": income,
+        }
+    })
+print("-"*50)
+print(data, sep="\n")
+print("-"*50)
+while True:
+    id_gir= input("zam yapılacak çalışanın numarasını giriniz: ")
+    if not id_gir:
+        break
+    if id_gir in data:
+        mevcut_maas= data[id_gir]["maaş"]
+        print(f"{id_no}numaralı kişinin şu anki maaşı:  {mevcut_maas}")
+        zam_orani= float(input("%'kaç zam yapılmasını istersiniz: "))
+        yeni_maas=mevcut_maas+mevcut_maas*(zam_orani/100)
+        data[id_gir]["maaş"]= yeni_maas
+        print(f"{id_gir} numaralı kişini yeni maaşı: {yeni_maas}")
+    else:
+        print("Geçerli id girmediniz")
+print("-"*50)        
+print(data, sep="\n")
+print("-"*50)
+
+
 
 
